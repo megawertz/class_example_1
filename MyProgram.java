e57ecfdd56b8d1073b682b6cfb99addb01ed6b09
Jason Wertz <jason.wertz@gmail.com>
2018-11-28 13:20:43 -0500

    Update MyProgram.java
    Create conflict on remote

--- MyProgram.java
+++ MyProgram.java
@@ -6,7 +6,7 @@ public class MyProgram
       // Note it will disappear when I switch back to
       //   my main branch.
       
-      // I made this change on the server but this could've been made by a colleague
+      // I made this change THIS WILL BE A CONFLICT SAME LINE EDITED IN TWO DIFFERENT PLACES could've been made by a colleague
    
    }
    
